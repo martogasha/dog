5ad1a5c2a0acec966f29e9b4960715ff2745ce23
martogasha <m.gachanga97@gmail.com>
2021-12-18 14:54:17 +0300

    first cosdfgssdfdsfd

--- database/migrations/2021_12_18_095203_create_products_table.php
+++ database/migrations/2021_12_18_095203_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('desc', 255)->change();
+            $table->char('desc');
             $table->integer('amount');
             $table->mediumText('image');
             $table->mediumText('image1')->nullable();
